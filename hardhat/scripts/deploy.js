4e83094402b7d8ebb133c21a133cc768705e5d8f
4Kirubha <jaimelannister4221@gmail.com>
2023-06-13 14:35:50 +0530

    changed constructor arguments

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -8,7 +8,7 @@ async function main(){
   console.log("RED TOKEN ADDRESS", deployedRedTokenContract.address);
 
   const stakingContract = await ethers.getContractFactory("Staking");
-  const deployedStakingContract = await stakingContract.deploy("0xecd0Eba51Fb95e8BE42eEE3489C157Ea91ed5965");
+  const deployedStakingContract = await stakingContract.deploy(deployedRedTokenContract.address);
   await deployedStakingContract.deployed();
   console.log("STAKING CONTRACT ADDRESS", deployedStakingContract.address);
 }
